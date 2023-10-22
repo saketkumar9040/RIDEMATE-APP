5b992a8b595f607bc05575acc5028a624e9d93f5
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-22 18:33:48 +0530

    feat - started with getting address from lat long using expo location pkg

--- src/navigations/mainNavigator.js
+++ src/navigations/mainNavigator.js
@@ -1,11 +1,36 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
+import * as Location from 'expo-location';
+
 import HomeScreen from "../screens/HomeScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
+import { Alert } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
+
+  const [location,setLocation]=useState(null);
+  const [address,setAddress]= useState(null);
+  console.log(address);
+
+  useEffect(()=>{
+    const startBackgroundTracking = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission for location was denied")
+       return;
+      }else{
+        let currentLocation = await Location.getCurrentPositionAsync({});
+        setLocation(currentLocation);
+        let currentAddress = await Location.reverseGeocodeAsync({latitude:currentLocation.coords.latitude,longitude:currentLocation.coords.longitude});
+        setAddress(currentAddress)
+      }
+    };
+    startBackgroundTracking();
+  },[]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
