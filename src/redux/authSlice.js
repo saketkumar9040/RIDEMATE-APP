f2ba7376a5c37fbbca5e24c05a3f2ca0dff07deb
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-24 16:22:16 +0530

    feat - redux added completed

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,2 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        userData:null,
+        currentLocation:null
+    },
+    reducers:{
+        setUserData:(state,action)=>{
+            state.userData=action.payload.userData;
+        },
+        setCurrentLocation:(state,action)=>{
+            state.currentLocation=action.payload.currentLocation;
+        }
+    }
+});
+
+export const authReducer = authSlice.reducer;
+export const {setCurrentLocation,setUserData}  = authSlice.actions;
