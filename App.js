973de88f468d9bdc4f1a35e3ec48ff1bd7d34032
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-24 16:15:41 +0530

    feat - implementing redux store

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
+import { Provider } from "react-redux";
 import MainNavigator from "./src/navigations/mainNavigator";
+import Store from "./src/redux/store";
 
 export default function App() {
-  return <MainNavigator />;
+  return (
+  <Provider store={Store}>
+    <MainNavigator />
+    </Provider>);
 }
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,10 +1,13 @@
 import { View, Text, StyleSheet, StatusBar } from "react-native";
 import React from "react";
 import MapView from "react-native-maps";
+import * as Location from "expo-location"
 
 import styles from "./style";
 
 const Map = () => {
+ let currentLocation =  Location.getCurrentPositionAsync({});
+ console.log(currentLocation)
   return (
     <View style={styles.mapContainer}>
       <StatusBar
@@ -13,7 +16,15 @@ const Map = () => {
         translucent
       />
       <Text style={styles.rideMateText}>RideMate</Text>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 };
