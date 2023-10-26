e891695f091125bb690fe89906a13cf9dcb8bd7c
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-27 01:04:11 +0530

    feat - direction from origin to destinatin set successfully

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -25,7 +25,7 @@ const Map = () => {
       return
     };
     mapRef.current.fitToSuppliedMarkers(["origin","destination"],{
-      edgePadding:{top:50,right:50,bottom:50,left:50}
+      edgePadding:{top:100,right:100,bottom:100,left:100}
     });
   },[startingPoint,destination]);
 
@@ -63,8 +63,8 @@ const Map = () => {
       
         <Marker
           coordinate={{
-            latitude: startingPoint?.location?.lat ,
-            longitude: startingPoint?.location?.lng ,
+            latitude: startingPoint.location.lat ,
+            longitude: startingPoint.location.lng ,
           }}
           title="origin"
           description={startingPoint?.description?? currentAddress.name}
