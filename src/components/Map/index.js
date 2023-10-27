3b66e1ba88a6d851f76fdb64a2e9950d808fec61
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-10-27 16:39:42 +0530

    refactor - working with calculation travel time

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,31 +1,47 @@
 import { View, Text, StyleSheet, StatusBar, Image } from "react-native";
 import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
-import MapViewDirections from 'react-native-maps-directions';
+import MapViewDirections from "react-native-maps-directions";
 import start from "../../../assets/images/pin.png";
 import end from "../../../assets/images/flag.png";
-import {GOOGLE_API_KEY} from "@env";
+import { GOOGLE_API_KEY } from "@env";
 
 import styles from "./style";
 import { useSelector } from "react-redux";
 import { mapCustomStyle } from "../../globals/styles/mapCustomStyle";
 
 const Map = () => {
-  const currentAddress = useSelector(state=>state.auth.currentAddress[0])
-  const startingPoint = useSelector(state=>state.nav.origin);
-  const destination = useSelector(state=>state.nav.destination);
+  const currentAddress = useSelector((state) => state.auth.currentAddress[0]);
+  const startingPoint = useSelector((state) => state.nav.origin);
+  const destination = useSelector((state) => state.nav.destination);
 
   const mapRef = useRef();
 
-  useEffect(()=>{
-    if(!startingPoint || ! destination){
-      return
-    };
-    mapRef.current.fitToSuppliedMarkers(["origin","destination"],{
-      edgePadding:{top:100,right:100,bottom:100,left:100}
+  useEffect(() => {
+    if (!startingPoint || !destination) {
+      return;
+    }
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 100, right: 100, bottom: 100, left: 100 },
     });
-  },[startingPoint,destination]);
+  }, [startingPoint, destination]);
 
+  useEffect(() => {
+    if (!startingPoint || !destination) {
+      return;
+    }
+    const getTravelTime = async () => {
+      const URL =
+        fetch(`https://maps.googleapis.com/maps/api/distancematrix/json
+      ?destinations=${destination.description}
+      &origins=${[`${startingPoint.location.lat},${startingPoint.location.lng}`]}
+      &units=imperial
+      &key=${GOOGLE_API_KEY}`)
+          .then((res) => console.log(res))
+         
+    };
+    getTravelTime();
+  }, [startingPoint, destination, GOOGLE_API_KEY]);
   return (
     <View style={styles.mapContainer}>
       <StatusBar
@@ -48,40 +64,49 @@ const Map = () => {
         showsUserLocation={true}
         followsUserLocation={true}
         rotateEnabled={true}
+        zoomEnabled={true}
         toolbarEnabled={true}
       >
-        {
-          startingPoint && destination && (
-            <MapViewDirections
-               origin={{latitude:startingPoint.location.lat,longitude:startingPoint.location.lng}}
-               destination={{latitude:destination.location.lat,longitude:destination.location.lng}}
-               apikey={GOOGLE_API_KEY}
-               strokeWidth={3}
-               strokeColor="black"
-            />
-          )
-        }
-      
+        {startingPoint && destination && (
+          <MapViewDirections
+            origin={{
+              latitude: startingPoint.location.lat,
+              longitude: startingPoint.location.lng,
+            }}
+            destination={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+            }}
+            apikey={GOOGLE_API_KEY}
+            strokeWidth={3}
+            strokeColor="black"
+          />
+        )}
+
         <Marker
           coordinate={{
-            latitude: startingPoint.location.lat ,
-            longitude: startingPoint.location.lng ,
+            latitude: startingPoint.location.lat,
+            longitude: startingPoint.location.lng,
           }}
           title="origin"
-          description={startingPoint?.description?? currentAddress.name}
+          description={startingPoint?.description ?? currentAddress.name}
           identifier="origin"
         >
           <Image
             source={start}
-            style={{ width: 34, height: 34, resizeMode: "contain",marginBottom:5, }}
+            style={{
+              width: 34,
+              height: 34,
+              resizeMode: "contain",
+              marginBottom: 5,
+            }}
           />
         </Marker>
-        {
-          destination && (
-            <Marker
+        {destination && (
+          <Marker
             coordinate={{
-              latitude: destination?.location.lat ,
-              longitude: destination?.location.lng ,
+              latitude: destination?.location.lat,
+              longitude: destination?.location.lng,
             }}
             title="destination"
             description={destination?.description}
@@ -92,8 +117,7 @@ const Map = () => {
               style={{ width: 34, height: 34, resizeMode: "contain" }}
             />
           </Marker>
-          )
-        }
+        )}
       </MapView>
     </View>
   );
